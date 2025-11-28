dbc74a4c4c27c086e9f8ec0b1d8c2d0913646b06
proneon267 <proneon267@gmail.com>
2025-11-28 10:21:12 -0800

    Import repo to github

---
+++ src/helloworld2/app.py
@@ -0,0 +1,32 @@
+"""
+My first application
+"""
+
+import toga
+from toga.style import Pack
+import asyncio
+
+
+class HelloWorld(toga.App):
+    async def cycle_states(self, *args, **kwargs):
+        for state in [*toga.constants.WindowState, toga.constants.WindowState.NORMAL]:
+            self.main_window.state = state
+            status_text = f"Current State: {self.main_window.state} | Current Size: {self.main_window.size}"
+            self.app.widgets["status_label"].text = status_text
+            print(status_text)
+            await asyncio.sleep(2)
+        self.app.exit()
+    def handle_button(self,*args,**kwargs):
+        self.webview._impl.send_message("hi")
+    def startup(self):
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.webview = toga.WebView()
+        self.send_button = toga.Button(text="Send", on_press=self.handle_button)
+        self.main_window.content = toga.Box(children=[self.send_button,self.webview])
+        self.main_window.show()
+
+        # asyncio.create_task(self.cycle_states())
+
+
+def main():
+    return HelloWorld()
