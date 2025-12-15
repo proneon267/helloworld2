d43b4b7010a38b25d76d2e705738d350b593d719
proneon267 <proneon267@gmail.com>
2025-12-15 05:18:19 -0800

    Update repo

--- src/helloworld2/app.py
+++ src/helloworld2/app.py
@@ -16,13 +16,26 @@ class HelloWorld(toga.App):
             print(status_text)
             await asyncio.sleep(2)
         self.app.exit()
-    def handle_button(self,*args,**kwargs):
-        self.webview._impl.send_message("hi")
+
+    async def handle_enable(self, *args, **kwargs):
+        await self.webview._impl.bridge.enable_bridge()
+
+    async def handle_disable(self, *args, **kwargs):
+        await self.webview.bridge.disable_bridge()
+
+    def handle_button(self, *args, **kwargs):
+        self.webview.bridge._impl.send_message("hi")
+
     def startup(self):
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.webview = toga.WebView()
         self.send_button = toga.Button(text="Send", on_press=self.handle_button)
-        self.main_window.content = toga.Box(children=[self.send_button,self.webview])
+        self.enable_btn = toga.Button(text="Enable", on_press=self.handle_enable)
+        self.disable_btn = toga.Button(text="Disable", on_press=self.handle_disable)
+
+        self.main_window.content = toga.Box(
+            children=[self.enable_btn, self.send_button, self.disable_btn, self.webview]
+        )
         self.main_window.show()
 
         # asyncio.create_task(self.cycle_states())
